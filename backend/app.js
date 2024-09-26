119414f12912225ffcfd78aec20fc2cb4e5ca951
avivheldman <42086900+avivheldman@users.noreply.github.com>
2024-09-26 15:36:20 +0000

    atals db

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 // Connect to mongo
 //TODO: (need change the address to the mongo-server toi an env var for generic deployment)
-mongo.connect('mongodb://0.0.0.0:27017/plants', { useNewUrlParser: true, useUnifiedTopology: true })
+mongo.connect('mongodb+srv://<menimamtera>:<Meni1234>@cluster0.likp8.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB Server: \n', err));
 
