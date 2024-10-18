076cb7fef55fa719cf8af7ae4acd793bcc9765a4
ofekSaar <ofeksaar87@gmail.com>
2024-10-18 20:22:36 +0300

    add to header and gitignore

--- ui/js/header.js
+++ ui/js/header.js
@@ -41,16 +41,13 @@ class Header {
         return `
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="#">My E-Commerce Store</a>
+                <a class="navbar-brand" href="index.html">Plants Store</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link ${this.activeLink === 'home' ? 'active' : ''}" href="index.html">Home</a>
-                        </li>
                         <li class="nav-item">
                             <a class="nav-link ${this.activeLink === 'cart' ? 'active' : ''}" href="cart.html">Cart (<span id="cart-count">0</span>)</a>
                         </li>
