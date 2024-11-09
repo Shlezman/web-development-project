12a4c6261091b34d1e78e51d7b8961da08000deb
Shlezman <117953382+Shlezman@users.noreply.github.com>
2024-11-09 17:26:48 +0200

    Update tweets.js

--- backend/routes/tweets.js
+++ backend/routes/tweets.js
@@ -1,12 +1,12 @@
+//token: AAAAAAAAAAAAAAAAAAAAALCywgEAAAAAo8ck6DcSEPK9gYxsO5V%2BYsCKARA%3DGawYI0g1gWkqo8d0FfCb4F7qNwXrWxckP3igTn7mH0jS36yDib
 const express = require('express');
-const auth = require('../middleware/auth');
 const router = express.Router();
 const asyncHandler = require("../utils/asyncHandler");
 
 // twitter auth
 const { TwitterApi } = require('twitter-api-v2');
 
-const twitterClient = new TwitterApi({ //token: AAAAAAAAAAAAAAAAAAAAALCywgEAAAAAo8ck6DcSEPK9gYxsO5V%2BYsCKARA%3DGawYI0g1gWkqo8d0FfCb4F7qNwXrWxckP3igTn7mH0jS36yDib
+const twitterClient = new TwitterApi({
   appKey: "BP68Sfnuv52Hpb4BJZzfGvN6j",
   appSecret: "JWgymqDBRfsfhZTk8IsvR0ww2HijJkIQuaf2OJeBs2lA5Oh4Aj",
   accessToken: "1851239489143705600-RklI6NyVvGaRYjs0a5gk3t1bU6LoR3",
@@ -14,22 +14,20 @@ const twitterClient = new TwitterApi({ //token: AAAAAAAAAAAAAAAAAAAAALCywgEAAAAA
 });
 
 
-// Tweet Endpoint
-router.post('/', [auth], asyncHandler(async (req, res) => {
+
+router.post('/', asyncHandler(async (req, res) => {
     try {
       const { tweet } = req.body;
   
-      // Validate tweet length (Twitter's max is 500 characters)
-      if (!tweet || tweet.length > 500) {
+      // Validate tweet length
+      if (!tweet || tweet.length > 280) {
         return res.status(400).json({ 
-          error: 'Tweet must be between 1 and 500 characters' 
+          error: 'Tweet must be between 1 and 280 characters' 
         });
       }
   
-      // Post the tweet
       const response = await twitterClient.v2.tweet(tweet);
   
-      // Send successful response
       res.status(200).json({ 
         message: 'Tweet posted successfully', 
         tweetId: response.data.id 
