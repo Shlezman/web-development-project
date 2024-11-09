cf09d84852c4e524ef06a2c0bbc422c8fdff3845
Shlezman <omri.shlezinger@gmail.com>
2024-11-09 23:49:06 +0200

    no tokens + google maps route

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,6 @@ app.use(cookieParser());
 
 // Load environment variables
 dotenv.config();
-
 // Body Parser
 app.use(bodyParser.json());
 
@@ -36,15 +35,15 @@ app.use(cors(corsOptions));
 
 // Connect to mongo
 //TODO: (need change the address to the mongo-server toi an env var for generic deployment)
-mongo.connect('mongodb+srv://menimamtera:Meni1234@cluster0.likp8.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+mongo.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.likp8.mongodb.net/`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB Server: \n', err));
 
-// Routes (need to add!)
 app.use('/api/users', require('./routes/users'));
 app.use('/api/plants', require('./routes/plants'));
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/tweet', require('./routes/tweets'));
+app.use('/api/map', require('./routes/maps'))
 
 // Start 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
